4a8c2f717b7f3d7b014cd40ade7db6fe6ef7da01
Sergio Medeiros <sergiof11@outlook.com.br>
2022-10-29 11:38:03 -0300

    Organizing files

---
+++ Projeto-2/src/Componentes/Card/index.jsx
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+function Card(props) {
+  return <div className="card"></div>;
+}
+
+export default Card;
---
+++ Projeto-2/src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+import Card from "../../Componentes/Card";
+
+function Home() {
+  return (
+    <div className="container">
+      <body>
+        <h1>Project Starter</h1>
+      </body>
+    </div>
+  );
+}
+
+export default Home;
