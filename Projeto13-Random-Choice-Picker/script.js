0fa5fd0b644c0540b523dd4bbacd98249cdf881e
Sergio Medeiros <sergiof11@outlook.com.br>
2022-11-12 15:27:30 -0300

    Random Choice Picker project finished

--- Projeto13-Random-Choice-Picker/script.js
+++ Projeto13-Random-Choice-Picker/script.js
@@ -1,10 +1,18 @@
 const tagsEl = document.getElementById("tags");
 const textarea = document.getElementById("textarea");
 
-textarea.focus(); // Ao entrar na página ele coloca o cursor direto para o textarea
+textarea.focus();
 
 textarea.addEventListener("keyup", (e) => {
-  createTags(e.target.value); // O que eu escrevo aparece no console
+  createTags(e.target.value);
+
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+
+    randomSelect();
+  }
 });
 
 function createTags(input) {
@@ -15,11 +23,49 @@ function createTags(input) {
 
   tagsEl.innerHTML = "";
 
-  // Lista do que você escreveu
   tags.forEach((tag) => {
     const tagEl = document.createElement("span");
     tagEl.classList.add("tag");
     tagEl.innerText = tag;
     tagsEl.appendChild(tagEl);
   });
+}
+
+function randomSelect() {
+  const times = 30;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    if (randomTag !== undefined) {
+      highlightTag(randomTag);
+
+      setTimeout(() => {
+        unHighlightTag(randomTag);
+      }, 100);
+    }
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+
+      highlightTag(randomTag);
+    }, 100);
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add("highlight");
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove("highlight");
 }
